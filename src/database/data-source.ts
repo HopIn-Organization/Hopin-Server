670cfbd3b22ef7df8a0e7463ca951a8401aa61ad
TamarTal23 <212927487@idf.il>
2026-03-26 16:00:03 +0200

    fix(database): remove default values for database connection parameters

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
+  username: process.env.DB_USERNAME ,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'hopin',
+  database: process.env.DB_NAME ,
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
   entities: ['src/database/entities/**/*.ts'],
